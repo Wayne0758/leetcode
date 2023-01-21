d6226d08942fe1f1a0a84396c343e7881e946df7
Wayne0758 <120694819+Wayne0758@users.noreply.github.com>
2023-01-21 20:55:37 +0000

    [LeetCode Sync] Runtime - 46 ms (5.28%), Memory - 13.4 MB (7.25%)

---
+++ Wayne0758/leetcode/2137-final-value-of-variable-after-performing-operations/solution.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def finalValueAfterOperations(self, operations):
+        """
+        :type operations: List[str]
+        :rtype: int
+        """
+        x=0
+        for i in operations:
+            if i[1]=="+":
+                x+=1
+            else:
+                x-=1
+        return x
+
