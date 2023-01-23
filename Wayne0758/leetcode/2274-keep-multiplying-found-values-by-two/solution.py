3b676b85409cc23812deb7c046aaa5c8f7157541
Wayne0758 <120694819+Wayne0758@users.noreply.github.com>
2023-01-23 07:10:46 +0000

    [LeetCode Sync] Runtime - 50 ms (10.26%), Memory - 13.8 MB (20.51%)

---
+++ Wayne0758/leetcode/2274-keep-multiplying-found-values-by-two/solution.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def findFinalValue(self, nums, original):
+        """
+        :type nums: List[int]
+        :type original: int
+        :rtype: int
+        """
+        while True:
+            if original in nums:
+                original *=2
+            else:
+                return original
