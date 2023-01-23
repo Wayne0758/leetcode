f1c80e0ff5a549b12970f69e3819940a01e2de71
Wayne0758 <120694819+Wayne0758@users.noreply.github.com>
2023-01-23 09:12:03 +0000

    Sync LeetCode submission - Intersection of Two Arrays (python)

---
+++ Wayne0758/leetcode/problems/intersection_of_two_arrays/solution.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        a=[]
+        for i in range(len(nums1)):
+            if nums1[i] in nums2 and nums1[i] not in a:
+                a.append(nums1[i])
+        return a
