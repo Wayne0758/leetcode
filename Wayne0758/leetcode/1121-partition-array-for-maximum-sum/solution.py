42d1d953fb0cb3f892eefecf1e4ba2847ffc7916
Wayne0758 <120694819+Wayne0758@users.noreply.github.com>
2024-02-03 14:25:49 +0000

    [LeetCode Sync] Runtime - 295 ms (47.73%), Memory - 11.6 MB (82.58%)

---
+++ Wayne0758/leetcode/1121-partition-array-for-maximum-sum/solution.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def maxSumAfterPartitioning(self, arr, k):
+        """
+        :type arr: List[int]
+        :type k: int
+        :rtype: int
+        """
+        n=len(arr)
+        dp=[0] * (n+1)
+        for i in range(1,n+1):
+            curMax=0
+            for j in range(1,k+1):
+                if j<=k and i-j>=0:
+                    curMax=max(curMax, arr[i-j])
+                    dp[i]=max(dp[i], dp[i-j]+curMax * j)
+        return dp[n]
