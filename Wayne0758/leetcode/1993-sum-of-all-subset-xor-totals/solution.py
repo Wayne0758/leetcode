c32c53d9c438572e65a91dfcbe016af376346363
Wayne0758 <120694819+Wayne0758@users.noreply.github.com>
2024-05-21 10:14:34 +0000

    [LeetCode Sync] Runtime - 42 ms (72.16%), Memory - 16.5 MB (80.31%)

--- Wayne0758/leetcode/1993-sum-of-all-subset-xor-totals/solution.py
+++ Wayne0758/leetcode/1993-sum-of-all-subset-xor-totals/solution.py
@@ -1,16 +1,11 @@
-class Solution(object):
-    def subsetXORSum(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        c=0
-        for i in range(2**len(nums)):
-            a=bin(i)[2:]
-            a=a.zfill(len(nums))
-            b=0
-            for j in range(len(a)):
-                if a[j]=="1":
-                    b = b ^ nums[j]
-            c+=b
-        return c
+class Solution:
+    def subsetXORSum(self, nums: List[int]) -> int:
+        res = 0
+        a = 0
+        for num in nums:
+            a = a|num
+        a = bin(a)
+        for i in range(len(a)):
+            if a[i]=='1':
+                res += (2**(len(nums)-1))*(2**(len(a)-i-1))
+        return res
