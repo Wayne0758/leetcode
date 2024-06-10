c5a6872ac3c04bb5220cf85d6c276e067ca1a750
Wayne0758 <120694819+Wayne0758@users.noreply.github.com>
2024-06-10 23:05:57 +0000

    [LeetCode Sync] Runtime - 42 ms (35.82%), Memory - 16.5 MB (49.29%)

--- Wayne0758/leetcode/1137-height-checker/solution.py
+++ Wayne0758/leetcode/1137-height-checker/solution.py
@@ -1,12 +1,8 @@
-class Solution(object):
-    def heightChecker(self, heights):
-        """
-        :type heights: List[int]
-        :rtype: int
-        """
-        a=sorted(heights)
-        b=0
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        expected = sorted(heights)
+        res = 0
         for i in range(len(heights)):
-            if heights[i]!=a[i]:
-                b+=1
-        return b
+            if expected[i]!=heights[i]:
+                res+=1
+        return res
