602637030395d3e1b3aded56a0b580ba6385b629
Wayne0758 <120694819+Wayne0758@users.noreply.github.com>
2024-07-22 10:40:33 +0000

    [LeetCode Sync] Runtime - 497 ms (56.24%), Memory - 21.7 MB (74.62%)

--- Wayne0758/leetcode/1711-find-valid-matrix-given-row-and-column-sums/solution.py
+++ Wayne0758/leetcode/1711-find-valid-matrix-given-row-and-column-sums/solution.py
@@ -1,16 +1,26 @@
-class Solution(object):
-    def restoreMatrix(self, rowSum, colSum):
-        """
-        :type rowSum: List[int]
-        :type colSum: List[int]
-        :rtype: List[List[int]]
-        """
-        m=len(rowSum)
-        n=len(colSum)
-        a=[[0 for j in range(n)] for i in range(m)]
-        for i in range(m):
+class Solution:
+    def restoreMatrix(self, rowSum: List[int], colSum: List[int]) -> List[List[int]]:
+        m = len(rowSum)
+        n = len(colSum)
+        res = [[0]*n for _ in range(m)]
+        while True:
+            if sum(rowSum) == 0:
+                return res
+            minrownum, minrowindex = math.inf, -1
+            mincolnum, mincolindex = math.inf, -1
+            for i in range(m):
+                if rowSum[i] < minrownum and rowSum[i]!=0:
+                    minrownum = rowSum[i]
+                    minrowindex = i
             for j in range(n):
-                a[i][j]=min(rowSum[i],colSum[j])
-                rowSum[i]-=a[i][j]
-                colSum[j]-=a[i][j]
-        return a
+                if colSum[j] < mincolnum and colSum[j]!=0:
+                    mincolnum = colSum[j]
+                    mincolindex = j
+            if minrownum <= mincolnum:
+                res[minrowindex][mincolindex] = minrownum
+                rowSum[minrowindex] -= minrownum
+                colSum[mincolindex] -= minrownum
+            else:
+                res[minrowindex][mincolindex] = mincolnum
+                rowSum[minrowindex] -= mincolnum
+                colSum[mincolindex] -= mincolnum
