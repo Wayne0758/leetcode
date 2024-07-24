5ce8941c3bd03419a41c64e5d2b22e41272f2325
Wayne0758 <120694819+Wayne0758@users.noreply.github.com>
2024-07-24 08:54:07 +0000

    [LeetCode Sync] Runtime - 51 ms (49.95%), Memory - 16.5 MB (90.65%)

--- Wayne0758/leetcode/1741-sort-array-by-increasing-frequency/solution.py
+++ Wayne0758/leetcode/1741-sort-array-by-increasing-frequency/solution.py
@@ -1,15 +1,13 @@
-class Solution(object):
-    def frequencySort(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        nums.sort(key = lambda x:-x)
-        a=[]
-        for i in range(len(nums)):
-            a.append([nums[i],nums.count(nums[i])])
-        a.sort(key = lambda x:x[1])
-        b=[]
-        for i in range(len(a)):
-            b.append(a[i][0])
-        return b
+class Solution:
+    def frequencySort(self, nums: List[int]) -> List[int]:
+        hashmap = {}
+        res = []
+        for num in nums:
+            hashmap[num] = hashmap.get(num, 0)+1
+        print(hashmap)
+        sorted_decreasing_hashmap = dict(sorted(hashmap.items(), key = lambda x:x[0], reverse = True))
+        sorted_hashmap = dict(sorted(sorted_decreasing_hashmap.items(), key = lambda x:x[1]))
+        for key in sorted_hashmap.keys():
+            for i in range(sorted_hashmap[key]):
+                res.append(key)
+        return res
