ed566cc8e0455657a3b05400b7aaeece9650dce3
Wayne0758 <120694819+Wayne0758@users.noreply.github.com>
2024-08-11 14:14:32 +0000

    [LeetCode Sync] Runtime - 149 ms (70.35%), Memory - 16.5 MB (16.26%)

---
+++ Wayne0758/leetcode/1971-incremental-memory-leak/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def memLeak(self, memory1: int, memory2: int) -> List[int]:
+        time = 1
+        while True:
+
+            if time>memory1 and time>memory2:
+                return [time,memory1,memory2]
+            else:
+                if memory1>=memory2:
+                    memory1-=time
+                else:
+                    memory2-=time
+            time+=1
