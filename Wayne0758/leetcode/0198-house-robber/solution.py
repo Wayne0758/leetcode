5ea48c17e5d55145366e282de33b08fc3a0f7986
Wayne0758 <120694819+Wayne0758@users.noreply.github.com>
2024-09-17 06:44:16 +0000

    [LeetCode Sync] Runtime - 38 ms (42.06%), Memory - 16.4 MB (93.02%)

---
+++ Wayne0758/leetcode/0198-house-robber/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        n = len(nums)
+
+        rob_next_plus_one = 0
+        rob_next = nums[n-1]
+
+        for i in range(n-2,-1,-1):
+            cur = max(rob_next, rob_next_plus_one + nums[i])
+            rob_next_plus_one = rob_next
+            rob_next = cur
+        return rob_next
