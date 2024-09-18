a68c16a51d31a1a2d2f5e3ffe0e01c2806ba533a
Wayne0758 <120694819+Wayne0758@users.noreply.github.com>
2024-09-18 07:07:13 +0000

    [LeetCode Sync] Runtime - 373 ms (40.27%), Memory - 70.3 MB (20.13%)

---
+++ Wayne0758/leetcode/0176-second-highest-salary/solution.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
+    employee = employee.drop_duplicates(["salary"])
+    if len(employee["salary"]) < 2:
+        return pd.DataFrame({"SecondHighestSalary": [np.NaN]})
+    employee = employee.sort_values("salary", ascending=False)
+    employee.drop("id", axis=1, inplace=True)
+    employee.rename({"salary": "SecondHighestSalary"}, axis=1, inplace=True)
+    return employee.head(2).tail(1)
